dbaffbbbc6dc180d847783ca2f8051bc9cd93153
GoranVukmanovic <goran.vukmanovic123@gmail.com>
2023-05-23 00:37:11 +0200

    mali clean up i prorade tipke

--- monopoly.py
+++ monopoly.py
@@ -7,42 +7,43 @@ root =  Tk()
 root.geometry("900x600+200+200")
 root.resizable(False, False)
 
-#slika ploce
-framePloce = Frame(root, width=600, height=600)
-framePloce.pack()
-framePloce.place(anchor='nw', relx=0, rely=0)
-ploca = ImageTk.PhotoImage(Image.open("ploca.png"))
-label = Label(framePloce, image=ploca)
-label.pack()
-
-#buttoni za playere
-p1Button = Button(root)
-p1Button.config(text="1", font=("Calibri", 16)) #ide image= za sliku
-p2Button = Button(root)
-p2Button.config(text="2", font=("Calibri", 16))
-p3Button = Button(root)
-p3Button.config(text="3", font=("Calibri", 16))
-p4Button = Button(root)
-p4Button.config(text="4", font=("Calibri", 16))
-
-p1Button.place(x=610, y=10)
-p2Button.place(x=690, y=10)
-p3Button.place(x=770, y=10)
-p4Button.place(x=850, y=10)
-
-
-#pokazi profile ljudi
-def showPlayerStats(igrac):
-    frameStats = Frame(root, width=250, height=300)
+#pokazi profile ljudi, svaki ima zasebnu funkciju jer jbg, bitno da radi
+#TODO uljepsat ak se oce
+def showPlayerStats_1():
+    frameStats = Frame(root, width = 250, height = 300)
     frameStats.pack()
-    frameStats.place(anchor='center', relx=0.83, rely=0.25)
-    stats = "Pare: " + str(igrac["pare"]) + "\nVlasnistvo: " + str(igrac["vlasnistvo"])
+    frameStats.place(anchor = 'center', relx = 0.83, rely = 0.25)
+    stats = "Pare: " + str(p1["pare"]) + "\nVlasnistvo: " + str(p1["vlasnistvo"])
+    labelStats = Label(frameStats, text=stats, font=("Calibri", 16))
+    labelStats.pack()
+
+def showPlayerStats_2():
+    frameStats = Frame(root, width = 250, height = 300)
+    frameStats.pack()
+    frameStats.place(anchor = 'center', relx = 0.83, rely = 0.25)
+    stats = "Pare: " + str(p2["pare"]) + "\nVlasnistvo: " + str(p2["vlasnistvo"])
+    labelStats = Label(frameStats, text=stats, font=("Calibri", 16))
+    labelStats.pack()
+
+def showPlayerStats_3():
+    frameStats = Frame(root, width = 250, height = 300)
+    frameStats.pack()
+    frameStats.place(anchor = 'center', relx = 0.83, rely = 0.25)
+    stats = "Pare: " + str(p3["pare"]) + "\nVlasnistvo: " + str(p3["vlasnistvo"])
     labelStats = Label(frameStats, text=stats, font=("Calibri", 16))
     labelStats.pack()
     
-#pokazi rezultat kocke
+def showPlayerStats_4():
+    frameStats = Frame(root, width = 250, height = 300)
+    frameStats.pack()
+    frameStats.place(anchor = 'center', relx = 0.83, rely = 0.25)
+    stats = "Pare: " + str(p4["pare"]) + "\nVlasnistvo: " + str(p4["vlasnistvo"])
+    labelStats = Label(frameStats, text=stats, font=("Calibri", 16))
+    labelStats.pack()
+
+#TODO pokazi rezultat kocke
 def showDice(diceNumber1, diceNumber2):
-    
+    pass
     
 
 #kocke duh
@@ -51,16 +52,39 @@ def kocke(igrac):
     diceNumber2 = random.randint(1,6)
     igrac["polje"] += (diceNumber1 + diceNumber2)
     if (igrac["polje"] >= 40):
-        igrac["polje"] == 0
+        igrac["polje"] = 0
         igrac["pare"] += 200
     showDice(diceNumber1, diceNumber2)
-    
+#slika ploce
+framePloce = Frame(root, width = 600, height = 600)
+framePloce.pack()
+framePloce.place(anchor = 'nw', relx = 0, rely = 0)
+ploca = ImageTk.PhotoImage(Image.open("ploca.png"))
+label = Label(framePloce, image=ploca)
+label.pack()
 
+  
 #igraci i pocetno stanje
-p1 = {"polje":0,"pare":1500,"vlasnistvo":[],"u_zatvoru":False,"bankrot":False}
-p2 = {"polje":0,"pare":1500,"vlasnistvo":[],"u_zatvoru":False,"bankrot":False}
-p3 = {"polje":0,"pare":1500,"vlasnistvo":[],"u_zatvoru":False,"bankrot":False}
-p4 = {"polje":0,"pare":1500,"vlasnistvo":[],"u_zatvoru":False,"bankrot":False}
+p1 = {"polje":0,"pare":1550,"vlasnistvo":[],"u_zatvoru":False,"bankrot":False}
+p2 = {"polje":0,"pare":1560,"vlasnistvo":[],"u_zatvoru":False,"bankrot":False}
+p3 = {"polje":0,"pare":1570,"vlasnistvo":[],"u_zatvoru":False,"bankrot":False}
+p4 = {"polje":0,"pare":1580,"vlasnistvo":[],"u_zatvoru":False,"bankrot":False}
 
-showPlayerStats(p2)
+# buttoni za playere, sve se moze na pocetku iskazat, netreba .config();
+#! command priziva funkciju, al neda da stavis args pa je zato 4 funkcije za ljude
+#TODO ide image= za sliku
+p1Button = Button(root, text = "1", font=("Calibri", 16), command = showPlayerStats_1)
+
+p2Button = Button(root, text = "2", font=("Calibri", 16), command = showPlayerStats_2)
+
+p3Button = Button(root, text = "3", font=("Calibri", 16), command = showPlayerStats_3)
+
+p4Button = Button(root, text = "4", font=("Calibri", 16), command = showPlayerStats_4)
+
+p1Button.place(x = 610, y = 10)
+p2Button.place(x = 690, y = 10)
+p3Button.place(x = 770, y = 10)
+p4Button.place(x = 850, y = 10)
+
+
 mainloop()
