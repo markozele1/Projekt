785e468bc1a7d86d9b32eb6c90ed613ae5962164
GoranVukmanovic <goran.vukmanovic123@gmail.com>
2023-05-26 00:44:17 +0200

    preraspodjela i sve nekoristene slike su u file slike, dodana main funkcija jer ce bit lakse napravit turnove koje jos nisam stigo, dodana zemlja u svojem .py file

--- monopoly.py
+++ monopoly.py
@@ -1,14 +1,23 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import random
+from zemlje import *
 
-# root window, not resizable
+#* root window, not resizable
 root =  Tk()
 root.geometry("900x600+200+200")
 root.resizable(False, False)
 
-#pokazi profile ljudi, svaki ima zasebnu funkciju jer jbg, bitno da radi
-#TODO uljepsat ak se oce
+#*igraci i pocetno stanje
+p1 = {"polje":0,"pare":1500,"vlasnistvo":["1"],"u_zatvoru":False,"bankrot":False}
+p2 = {"polje":0,"pare":1500,"vlasnistvo":["2"],"u_zatvoru":False,"bankrot":False}
+p3 = {"polje":0,"pare":1500,"vlasnistvo":["3"],"u_zatvoru":False,"bankrot":False}
+p4 = {"polje":0,"pare":1500,"vlasnistvo":["4"],"u_zatvoru":False,"bankrot":False}
+
+players = [p1,p2,p3,p4]
+
+#*pokazi profile ljudi, svaki ima zasebnu funkciju jer jbg, bitno da radi
+#TODO uljepsat ovo sranje
 def showPlayerStats_1():
     frameStats = Frame(root, width = 250, height = 300)
     frameStats.pack()
@@ -41,13 +50,9 @@ def showPlayerStats_4():
     labelStats = Label(frameStats, text=stats, font=("Calibri", 16))
     labelStats.pack()
 
-#TODO pokazi rezultat kocke
-def showDice(diceNumber1, diceNumber2):
-    pass
-    
 
-#kocke duh
-def kocke(igrac):
+#*kocke duh
+def dice(igrac):
     diceNumber1 = random.randint(1,6)
     diceNumber2 = random.randint(1,6)
     igrac["polje"] += (diceNumber1 + diceNumber2)
@@ -55,36 +60,38 @@ def kocke(igrac):
         igrac["polje"] = 0
         igrac["pare"] += 200
     showDice(diceNumber1, diceNumber2)
-#slika ploce
-framePloce = Frame(root, width = 600, height = 600)
-framePloce.pack()
-framePloce.place(anchor = 'nw', relx = 0, rely = 0)
-ploca = ImageTk.PhotoImage(Image.open("ploca.png"))
-label = Label(framePloce, image=ploca)
-label.pack()
 
-  
-#igraci i pocetno stanje
-p1 = {"polje":0,"pare":1550,"vlasnistvo":[],"u_zatvoru":False,"bankrot":False}
-p2 = {"polje":0,"pare":1560,"vlasnistvo":[],"u_zatvoru":False,"bankrot":False}
-p3 = {"polje":0,"pare":1570,"vlasnistvo":[],"u_zatvoru":False,"bankrot":False}
-p4 = {"polje":0,"pare":1580,"vlasnistvo":[],"u_zatvoru":False,"bankrot":False}
-
-# buttoni za playere, sve se moze na pocetku iskazat, netreba .config();
-#! command priziva funkciju, al neda da stavis args pa je zato 4 funkcije za ljude
-#TODO ide image= za sliku
-p1Button = Button(root, text = "1", font=("Calibri", 16), command = showPlayerStats_1)
-
-p2Button = Button(root, text = "2", font=("Calibri", 16), command = showPlayerStats_2)
-
-p3Button = Button(root, text = "3", font=("Calibri", 16), command = showPlayerStats_3)
-
-p4Button = Button(root, text = "4", font=("Calibri", 16), command = showPlayerStats_4)
-
-p1Button.place(x = 610, y = 10)
-p2Button.place(x = 690, y = 10)
-p3Button.place(x = 770, y = 10)
-p4Button.place(x = 850, y = 10)
+#TODO pokazi rezultat kocke
+def showDice(diceNumber1, diceNumber2):
+    pass
+    
+#* sve je roknuto u main jer je tak lakse neke stvari napravit
+def main():
+    #*slika ploce
+    framePloce = Frame(root, width = 600, height = 600)
+    framePloce.pack()
+    framePloce.place(anchor = 'nw', relx = 0, rely = 0)
+    ploca = ImageTk.PhotoImage(Image.open("ploca.png"))
+    label = Label(framePloce, image=ploca)
+    label.pack()
 
 
-mainloop()
+    #* buttoni za playere, sve se moze na pocetku iskazat, netreba .config();
+    #! command priziva funkciju, al neda da stavis args pa je zato 4 funkcije za ljude
+    #TODO ide image= za sliku
+    p1Button = Button(root, text = "1", font=("Calibri", 16), command = showPlayerStats_1)
+    p2Button = Button(root, text = "2", font=("Calibri", 16), command = showPlayerStats_2)
+    p3Button = Button(root, text = "3", font=("Calibri", 16), command = showPlayerStats_3)
+    p4Button = Button(root, text = "4", font=("Calibri", 16), command = showPlayerStats_4)
+    #* placeanje koordinata za buttone
+    p1Button.place(x = 610, y = 10)
+    p2Button.place(x = 690, y = 10)
+    p3Button.place(x = 770, y = 10)
+    p4Button.place(x = 850, y = 10)
+
+    mainloop()
+#* mjesto gdje main pocinje
+#TODO NAPRAVI NES JBT, probaj 
+if(__name__ == "__main__"):
+    test_fn() #!provjerava jel radi file link
+    main()
---
+++ zemlje.py
@@ -0,0 +1,282 @@
+
+#* zemlje
+#?svaka zemlja ima 9 stvari za pratiti:
+#?cjena, cjenaKuce, renta, 1kuca, 2kuca, 3kuca, 4kuca, hotel i vlasnika
+#?cjena će uvjek bit ponuđena osim ako vlasnik nije None, cjenaKuce je univerzalna kolko ce kuce doci, 
+#?sve cjene koje se daju drugima tj. renta,1kuca...hotel imaju True/False da se lakse nade tj. trazit ce se koji je True i onda nabit cjena
+mediterranean_ave = {"cjena":60,
+                     "cjenaKuce":50,
+                     "renta":[False,2],
+                     "1kuca":[False,10],
+                     "2kuca":[False,30],
+                     "3kuca":[False,90],
+                     "4kuca":[False,160],
+                     "hotel":[False,250],
+                     "vlasnik":None}
+baltic_ave = {"cjena":60,
+                     "cjenaKuce":50,
+                     "renta":[False,4],
+                     "1kuca":[False,20],
+                     "2kuca":[False,60],
+                     "3kuca":[False,180],
+                     "4kuca":[False,320],
+                     "hotel":[False,450],
+                     "vlasnik":None}
+oriental_ave = {"cjena":100,
+                     "cjenaKuce":50,
+                     "renta":[False,6],
+                     "1kuca":[False,30],
+                     "2kuca":[False,90],
+                     "3kuca":[False,270],
+                     "4kuca":[False,400],
+                     "hotel":[False,550],
+                     "vlasnik":None}
+vermont_ave = {"cjena":100,
+                     "cjenaKuce":50,
+                     "renta":[False,6],
+                     "1kuca":[False,30],
+                     "2kuca":[False,90],
+                     "3kuca":[False,270],
+                     "4kuca":[False,400],
+                     "hotel":[False,550],
+                     "vlasnik":None}
+connecticut_ave = {"cjena":120,
+                     "cjenaKuce":50,
+                     "renta":[False,8],
+                     "1kuca":[False,40],
+                     "2kuca":[False,100],
+                     "3kuca":[False,300],
+                     "4kuca":[False,450],
+                     "hotel":[False,600],
+                     "vlasnik":None}
+st_charles = {"cjena":140,
+                     "cjenaKuce":100,
+                     "renta":[False,10],
+                     "1kuca":[False,50],
+                     "2kuca":[False,150],
+                     "3kuca":[False,450],
+                     "4kuca":[False,625],
+                     "hotel":[False,750],
+                     "vlasnik":None}
+states_ave = {"cjena":140,
+                     "cjenaKuce":100,
+                     "renta":[False,10],
+                     "1kuca":[False,50],
+                     "2kuca":[False,150],
+                     "3kuca":[False,450],
+                     "4kuca":[False,625],
+                     "hotel":[False,750],
+                     "vlasnik":None}
+virginia_ave = {"cjena":160,
+                     "cjenaKuce":100,
+                     "renta":[False,12],
+                     "1kuca":[False,60],
+                     "2kuca":[False,180],
+                     "3kuca":[False,500],
+                     "4kuca":[False,700],
+                     "hotel":[False,900],
+                     "vlasnik":None}
+st_james = {"cjena":180,
+                     "cjenaKuce":100,
+                     "renta":[False,14],
+                     "1kuca":[False,70],
+                     "2kuca":[False,200],
+                     "3kuca":[False,550],
+                     "4kuca":[False,750],
+                     "hotel":[False,950],
+                     "vlasnik":None}
+tennesssee_ave = {"cjena":180,
+                     "cjenaKuce":100,
+                     "renta":[False,14],
+                     "1kuca":[False,70],
+                     "2kuca":[False,200],
+                     "3kuca":[False,550],
+                     "4kuca":[False,750],
+                     "hotel":[False,950],
+                     "vlasnik":None}
+newyork_ave = {"cjena":200,
+                     "cjenaKuce":100,
+                     "renta":[False,16],
+                     "1kuca":[False,80],
+                     "2kuca":[False,220],
+                     "3kuca":[False,600],
+                     "4kuca":[False,800],
+                     "hotel":[False,1000],
+                     "vlasnik":None}
+kentucky_ave = {"cjena":220,
+                     "cjenaKuce":150,
+                     "renta":[False,18],
+                     "1kuca":[False,90],
+                     "2kuca":[False,250],
+                     "3kuca":[False,700],
+                     "4kuca":[False,875],
+                     "hotel":[False,1050],
+                     "vlasnik":None}
+indiana_ave = {"cjena":220,
+                     "cjenaKuce":150,
+                     "renta":[False,18],
+                     "1kuca":[False,90],
+                     "2kuca":[False,250],
+                     "3kuca":[False,700],
+                     "4kuca":[False,875],
+                     "hotel":[False,1050],
+                     "vlasnik":None}
+illinois_ave = {"cjena":240,
+                     "cjenaKuce":150,
+                     "renta":[False,20],
+                     "1kuca":[False,100],
+                     "2kuca":[False,300],
+                     "3kuca":[False,750],
+                     "4kuca":[False,925],
+                     "hotel":[False,1100],
+                     "vlasnik":None}
+atlantic_ave = {"cjena":260,
+                     "cjenaKuce":150,
+                     "renta":[False,22],
+                     "1kuca":[False,110],
+                     "2kuca":[False,330],
+                     "3kuca":[False,800],
+                     "4kuca":[False,975],
+                     "hotel":[False,1150],
+                     "vlasnik":None}
+ventor_ave = {"cjena":260,
+                     "cjenaKuce":150,
+                     "renta":[False,22],
+                     "1kuca":[False,110],
+                     "2kuca":[False,330],
+                     "3kuca":[False,800],
+                     "4kuca":[False,975],
+                     "hotel":[False,1150],
+                     "vlasnik":None}
+marvin_gardens = {"cjena":280,
+                     "cjenaKuce":150,
+                     "renta":[False,24],
+                     "1kuca":[False,120],
+                     "2kuca":[False,360],
+                     "3kuca":[False,850],
+                     "4kuca":[False,1025],
+                     "hotel":[False,1200],
+                     "vlasnik":None}
+pacific_ave = {"cjena":300,
+                     "cjenaKuce":200,
+                     "renta":[False,26],
+                     "1kuca":[False,130],
+                     "2kuca":[False,390],
+                     "3kuca":[False,900],
+                     "4kuca":[False,1100],
+                     "hotel":[False,1275],
+                     "vlasnik":None}
+northcarolina_ave = {"cjena":300,
+                     "cjenaKuce":200,
+                     "renta":[False,26],
+                     "1kuca":[False,130],
+                     "2kuca":[False,390],
+                     "3kuca":[False,900],
+                     "4kuca":[False,1100],
+                     "hotel":[False,1275],
+                     "vlasnik":None}
+pennsylvania_ave = {"cjena":320,
+                     "cjenaKuce":200,
+                     "renta":[False,28],
+                     "1kuca":[False,150],
+                     "2kuca":[False,450],
+                     "3kuca":[False,1000],
+                     "4kuca":[False,1200],
+                     "hotel":[False,1400],
+                     "vlasnik":None}
+park_palace = {"cjena":350,
+                     "cjenaKuce":200,
+                     "renta":[False,35],
+                     "1kuca":[False,175],
+                     "2kuca":[False,500],
+                     "3kuca":[False,1100],
+                     "4kuca":[False,1300],
+                     "hotel":[False,1500],
+                     "vlasnik":None}
+boardwalk = {"cjena":400,
+                     "cjenaKuce":200,
+                     "renta":[False,50],
+                     "1kuca":[False,200],
+                     "2kuca":[False,600],
+                     "3kuca":[False,1400],
+                     "4kuca":[False,1700],
+                     "hotel":[False,2000],
+                     "vlasnik":None}
+#* kolodvori
+#?skoro isto ko zemlje samo što prati cjenu, rentu i vlasnika
+#?svaki dodatni tko netko posjeduje ce biti +25 na renti, najlakše je da samo tražimo tko sve što ima i onda pozbrajamo rente
+reading_rail = {"cjena":200,
+                "renta":25,
+                "vlasnik":None}
+pennsylvania_rail = {"cjena":200,
+                "renta":25,
+                "vlasnik":None}
+bo_rail = {"cjena":200,
+                "renta":25,
+                "vlasnik":None}
+short_rail = {"cjena":200,
+                "renta":25,
+                "vlasnik":None}
+#* utils
+#?samo cijena i vlasnik
+#!način rada je sjeban jer ako imas 1 onda ce renta biti vrijednos dice*4, ako dva onda je vrijednost dice*10
+electric = {"cjena":150,
+            "vlasnik":None}
+water = {"cjena":150,
+            "vlasnik":None}
+#* nonbuy stvari i ostala polja
+#?sve što počinje na nonbuy se nemre kupit jer je to dobar način za bugproof i to je uvijek na index[0]
+#?svaki će biti lista koja na index[1] ima string koji može služizi za aktiviranje funkcija
+start = ["non_buy","takeStart"]
+tax = ["nonbuy","tax"]
+luxtax = ["nonbuy","luxtax"]
+comm_chest = ["nonbuy","pickComm"]
+chance = ["nonbuy","pickChance"]
+jail = ["nonbuy","jailCheck"]
+goto_jail = ["nonbuy","gotoJail"]
+parking = ["nonbuy","parkingCheck"]
+#* lista svih polja
+polja = [start,
+         mediterranean_ave,
+         comm_chest,baltic_ave,
+         tax,
+         reading_rail,
+         oriental_ave,
+         chance,
+         vermont_ave,
+         connecticut_ave,
+         jail,
+         st_charles,
+         electric,
+         states_ave,
+         virginia_ave,
+         pennsylvania_rail,
+         st_james,
+         comm_chest,
+         tennesssee_ave,
+         newyork_ave,
+         parking,
+         kentucky_ave,
+         chance,
+         indiana_ave,
+         illinois_ave,
+         bo_rail,
+         atlantic_ave,
+         ventor_ave,
+         water,
+         marvin_gardens,
+         goto_jail,
+         pacific_ave,
+         northcarolina_ave,
+         comm_chest,
+         pennsylvania_ave,
+         short_rail,
+         chance,
+         park_palace,
+         luxtax,
+         boardwalk
+         ]
+#* test funkcija
+#? služi da ti pokaže ako se file spojio i treba printat sva polja
+def test_fn():
+    print(*polja)
