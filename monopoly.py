d493624b914c448b40627fb80d30fe9f09322da0
GoranVukmanovic <goran.vukmanovic123@gmail.com>
2023-05-31 16:03:56 +0200

    implementirano kretanje po ploci i turnovi za igrace, treba dodat kak rade s zemljistima i jos neke stvari pod TODO

--- monopoly.py
+++ monopoly.py
@@ -5,8 +5,17 @@ from zemlje import *
 
 #* root window, not resizable
 root =  Tk()
+root.title("Monopoly")
 root.geometry("900x600+200+200")
 root.resizable(False, False)
+
+#*slika ploce
+framePloce = Frame(root, width = 600, height = 600)
+framePloce.pack()
+framePloce.place(anchor = 'nw', relx = 0, rely = 0)
+ploca = ImageTk.PhotoImage(Image.open("ploca.png"))
+label = Label(framePloce, image=ploca)
+label.pack()
 
 #*igraci i pocetno stanje
 p1 = {"polje":0,"pare":1500,"vlasnistvo":["1"],"u_zatvoru":False,"bankrot":False}
@@ -14,52 +23,66 @@ p2 = {"polje":0,"pare":1500,"vlasnistvo":["2"],"u_zatvoru":False,"bankrot":False
 p3 = {"polje":0,"pare":1500,"vlasnistvo":["3"],"u_zatvoru":False,"bankrot":False}
 p4 = {"polje":0,"pare":1500,"vlasnistvo":["4"],"u_zatvoru":False,"bankrot":False}
 
-players = [p1,p2,p3,p4]
+#*lista u kojoj se spremaju igraci i varijabla u kojoj se prati trenutni igrac
+igraci = [p1,p2,p3,p4]
+trenutni_igrac = 0
 
-#*pokazi profile ljudi, svaki ima zasebnu funkciju jer jbg, bitno da radi
-#TODO uljepsat ovo sranje
-def showPlayerStats_1():
+#*pokazi profile ljudi, uljepsano
+def showPlayerStats(igrac):
     frameStats = Frame(root, width = 250, height = 300)
     frameStats.pack()
     frameStats.place(anchor = 'center', relx = 0.83, rely = 0.25)
-    stats = "Pare: " + str(p1["pare"]) + "\nVlasnistvo: " + str(p1["vlasnistvo"])
+    stats = "Pare: " + str(igrac["pare"]) + "\nVlasnistvo: " + str(igrac["vlasnistvo"]) + "\nPolje: " + str(igrac["polje"])
     labelStats = Label(frameStats, text=stats, font=("Calibri", 16))
     labelStats.pack()
 
-def showPlayerStats_2():
-    frameStats = Frame(root, width = 250, height = 300)
-    frameStats.pack()
-    frameStats.place(anchor = 'center', relx = 0.83, rely = 0.25)
-    stats = "Pare: " + str(p2["pare"]) + "\nVlasnistvo: " + str(p2["vlasnistvo"])
-    labelStats = Label(frameStats, text=stats, font=("Calibri", 16))
-    labelStats.pack()
-
-def showPlayerStats_3():
-    frameStats = Frame(root, width = 250, height = 300)
-    frameStats.pack()
-    frameStats.place(anchor = 'center', relx = 0.83, rely = 0.25)
-    stats = "Pare: " + str(p3["pare"]) + "\nVlasnistvo: " + str(p3["vlasnistvo"])
-    labelStats = Label(frameStats, text=stats, font=("Calibri", 16))
-    labelStats.pack()
-    
-def showPlayerStats_4():
-    frameStats = Frame(root, width = 250, height = 300)
-    frameStats.pack()
-    frameStats.place(anchor = 'center', relx = 0.83, rely = 0.25)
-    stats = "Pare: " + str(p4["pare"]) + "\nVlasnistvo: " + str(p4["vlasnistvo"])
-    labelStats = Label(frameStats, text=stats, font=("Calibri", 16))
-    labelStats.pack()
-
-
-#*kocke duh
+#*kocke, daju broj za kolko ici, premjestaju na sljedeceg igraca
 def dice(igrac):
+    global trenutni_igrac
     diceNumber1 = random.randint(1,6)
     diceNumber2 = random.randint(1,6)
+    #TODO ako igrac ode u zatvor
+    if(igrac["u_zatvoru"]):
+        poljeCheck(igrac)
     igrac["polje"] += (diceNumber1 + diceNumber2)
     if (igrac["polje"] >= 40):
-        igrac["polje"] = 0
+        igrac["polje"] -= 40
         igrac["pare"] += 200
+    poljeCheck(igrac)
+    if(trenutni_igrac < len(igraci)-1):
+        trenutni_igrac += 1
+    else:
+        trenutni_igrac = 0
     showDice(diceNumber1, diceNumber2)
+    showPlayerStats(igrac)
+
+#* provjerava trenutno polje na kojem se igrac nalazi
+def poljeCheck(igrac):
+    trenutno_polje = polja[igrac["polje"]]
+    if("nonbuy" in trenutno_polje):
+        poljeAction(igrac,trenutno_polje[1])
+    else:
+        #TODO kupnja nonbuy polja
+        print(trenutno_polje["ime"])
+
+#* provjerava posebne kartice
+#TODO napravit sve sto pise TODO u printovima
+def poljeAction(igrac,action):
+    if(action == "tax"):
+        igrac["pare"] -= 200
+    elif(action == "luxtax"):
+        igrac["pare"] -= 100
+    elif(action == "pickComm"):
+        print("TODO commchest karte")
+    elif(action == "pickChance"):
+        print("TODO chance karte")
+    elif(action == "jailCheck"):
+        print("TODO jail check")
+    elif(action == "gotoJail"):
+        igrac["polje"] = 10
+        igrac["u_zatvoru"] = True
+    elif(action == "parkingCheck"):
+        print("TODO parking")
 
 #TODO pokazi rezultat kocke
 def showDice(diceNumber1, diceNumber2):
@@ -67,29 +90,24 @@ def showDice(diceNumber1, diceNumber2):
     
 #* sve je roknuto u main jer je tak lakse neke stvari napravit
 def main():
-    #*slika ploce
-    framePloce = Frame(root, width = 600, height = 600)
-    framePloce.pack()
-    framePloce.place(anchor = 'nw', relx = 0, rely = 0)
-    ploca = ImageTk.PhotoImage(Image.open("ploca.png"))
-    label = Label(framePloce, image=ploca)
-    label.pack()
-
-
     #* buttoni za playere, sve se moze na pocetku iskazat, netreba .config();
     #! command priziva funkciju, al neda da stavis args pa je zato 4 funkcije za ljude
     #TODO ide image= za sliku
-    p1Button = Button(root, text = "1", font=("Calibri", 16), command = showPlayerStats_1)
-    p2Button = Button(root, text = "2", font=("Calibri", 16), command = showPlayerStats_2)
-    p3Button = Button(root, text = "3", font=("Calibri", 16), command = showPlayerStats_3)
-    p4Button = Button(root, text = "4", font=("Calibri", 16), command = showPlayerStats_4)
+    p1Button = Button(root, text = "1", font = ("Calibri", 16), command = lambda: showPlayerStats(p1))
+    p2Button = Button(root, text = "2", font = ("Calibri", 16), command = lambda: showPlayerStats(p2))
+    p3Button = Button(root, text = "3", font = ("Calibri", 16), command = lambda: showPlayerStats(p3))
+    p4Button = Button(root, text = "4", font = ("Calibri", 16), command = lambda: showPlayerStats(p4))
+    diceButton = Button(root,text = "Roll!", font = ("Calibri", 16), command = lambda: dice(igraci[trenutni_igrac]))
+    
     #* placeanje koordinata za buttone
     p1Button.place(x = 610, y = 10)
     p2Button.place(x = 690, y = 10)
     p3Button.place(x = 770, y = 10)
     p4Button.place(x = 850, y = 10)
-
+    diceButton.place(x = 610, y = 550)
     mainloop()
+
+
 #* mjesto gdje main pocinje
 #TODO NAPRAVI NES JBT, probaj 
 if(__name__ == "__main__"):
